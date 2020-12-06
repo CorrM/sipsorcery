a06c4c1da437f1f6378109793eb94b04704b94fc
Islam Nofl <islamnofl.official@gmail.com>
2020-12-06 03:25:02 +0200

    Update SIPTransactionEngine.cs

--- src/core/SIPTransactions/SIPTransactionEngine.cs
+++ src/core/SIPTransactions/SIPTransactionEngine.cs
@@ -561,13 +561,16 @@ namespace SIPSorcery.SIP
             // If there is no tx request then it must be a PRack request we're being asked to send reliably.
             SIPRequest req = transaction.TransactionRequest ?? transaction.PRackRequest;
 
-            if (transaction.OutboundProxy != null)
+            if (transaction.Retransmits == 1 || req.URI.Protocol == SIPProtocolsEnum.udp)
             {
-                result = m_sipTransport.SendRequestAsync(transaction.OutboundProxy, req);
-            }
-            else
-            {
-                result = m_sipTransport.SendRequestAsync(req);
+            	 if (transaction.OutboundProxy != null)
+            	{
+            		result = m_sipTransport.SendRequestAsync(transaction.OutboundProxy, req);
+            	}
+            	else
+            	{
+            		result = m_sipTransport.SendRequestAsync(req);
+            	}
             }
 
             return result;
